d4c716b970f87389da94587f4504dfdc1b2a6d32
Marina F <fedma001@mail.ru>
2022-02-14 12:17:08 +0300

    started saver

--- saver.py
+++ saver.py
@@ -1,1 +1,20 @@
-import generator_data as gd
+import view
+import controller
+from datetime import datetime as dt
+from time import time
+def save_first_name(data):
+    time = dt.now().strftime('%H:%M')
+    with open ('dictionary.txt', 'a') as file:
+        file.write('{}; first name;{}\n'
+                .format(time, data))
+def save_surname(data):
+    time = dt.now().strftime('%H:%M')
+    with open ('dictionary.txt', 'a') as file:
+        file.write('{}; surname;{}\n'
+                .format(time, data))
+def save_description(data):
+    time = dt.now().strftime('%H:%M')
+    with open ('dictionary.txt', 'a') as file:
+        file.write('{}; description;{}\n'
+                .format(time, data))
+
