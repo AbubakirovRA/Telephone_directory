b84bf23331457f24ef5525734af318b9266b1902
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-14 23:27:46 +0500

    Add base modules

--- reader.py
+++ reader.py
@@ -1,4 +1,4 @@
-def reader():
-    with open('dictionary.txt', 'r') as file:
-        for item in file:
-            print(item)
+def find_data(search_data):
+    with open('dictionary.txt', 'r', encoding='utf-8') as file:
+        lines = file.read().splitlines()
+    return lines
--- saver.py
+++ saver.py
@@ -1,20 +1,7 @@
-import view
-import controller
-from datetime import datetime as dt
-from time import time
-def save_first_name(data):
-    time = dt.now().strftime('%H:%M')
-    with open ('dictionary.txt', 'a') as file:
-        file.write('{}; first name;{}\n'
-                .format(time, data))
-def save_surname(data):
-    time = dt.now().strftime('%H:%M')
-    with open ('dictionary.txt', 'a') as file:
-        file.write('{}; surname;{}\n'
-                .format(time, data))
-def save_description(data):
-    time = dt.now().strftime('%H:%M')
-    with open ('dictionary.txt', 'a') as file:
-        file.write('{}; description;{}\n'
-                .format(time, data))
+def add_data(add_data):
+    return 0
+
+def save_data(save_data):
+    return 1
+
 
