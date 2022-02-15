80fbcad87e872a2d08dee0d7b0a6f523e4b180bc
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-15 23:19:28 +0500

    Add change to saver

--- controller.py
+++ controller.py
@@ -3,12 +3,13 @@ import saver
 import reader
 import generator_data
 
-def start_programm(user_choice):
+def start_programm():
+    user_choice = view.get_data('Enter your choice:\nfind value(f)\nadd value(f)\nimport data(i)\nexport data(e)\n:  ')
     if user_choice == 'f':
         view.view_data(reader.find_data(view.get_data('Enter data to search: ')), 'data')
     elif user_choice == 'a':
-        saver.save_data(view.get_data('Enter data to add: '))
+        saver.add_data(view.get_data('Enter data to add: '))
     elif user_choice == 'e':
         generator_data.export(view.get_data('Enter a file name to export to csv format: '))
     elif user_choice == 'i':
-        saver.add_data(view.get_data('Enter a file name for data import: '))
+        saver.save_data(view.get_data('Enter a file name for data import: '))
--- saver.py
+++ saver.py
@@ -1,5 +1,10 @@
+import controller as c
+
 def add_data(add_data):
-    return 0
+    with open('dictionary.txt', 'a', encoding = 'utf-8') as file:
+        file.write(f'{add_data}\n')
+    if input('Continue?  (y/any key) ').lower() == 'y':
+        c.start_programm()
 
 def save_data(save_data):
     return 1
