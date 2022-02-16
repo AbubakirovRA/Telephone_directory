f0f607dd5ce97cc7be1b8bc48f139c69aab9469a
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-16 23:32:33 +0500

    Add generator_data

--- generator_data.py
+++ generator_data.py
@@ -1,2 +1,14 @@
-def export(ex_data):
-    return 4
+def export(file_name):
+    style = 'style="font-size:30px;"'
+    html = '<html>\n  <head></head>\n  <body>\n'
+    d = {}
+    with open('dictionary.txt', 'r', encoding='utf-8') as file:
+        d = {" ".join(i.replace("\n", "").split(" ")) for i in file}
+    for j in d:
+        html += f'    {j} <br>\n'
+    html += '  </body>\n</html>'
+    
+    with open(file_name+'.html', 'w') as page:
+        page.write(html)
+
+    return html
